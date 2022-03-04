3287ef2ebb47e7a4b7c502a8740c75041fd71138
Victor Laragnoit <victor.laragnoit@gmail.com>
2022-03-04 19:02:51 -0300

    firt commit

---
+++ AULA03-EXERCICIOS/src/main/java/Exercicio1.java
@@ -0,0 +1,20 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        
+
+        int partida_01, partida_02, partida_03;
+
+        int total, media;
+        partida_01 = 130;
+        partida_02 = 240;
+        partida_03 = 320;
+
+        total = partida_01 + partida_02 + partida_03;
+        media = total/3;
+
+        System.out.println("Total de zumbis mortos: " + total);
+        System.out.println("Media de zumbis mortos: " + media);
+
+    }
+    
+}
