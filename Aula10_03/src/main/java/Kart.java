a7ad40f68c83511d3ed9ca1e20fc71e4ee9d79f6
VictorLaragnoit <victor.laragnoit@gmail.com>
2022-03-10 20:44:53 -0300

    new project

---
+++ Aula10_03/src/main/java/Kart.java
@@ -0,0 +1,20 @@
+public class Kart {
+
+    String nome;
+
+    void pular(){
+        System.out.println("Carro " + nome + "esta pulando!")
+
+    }
+
+    void soltarTurbo(){
+        System.out.println("Carro" + nome + "esta soltando turbo!")
+
+    }
+
+    void fazerDrift(){
+        System.out.println("Carro" + nome + "esta fazendo drift!")
+        
+    }
+    
+}
---
+++ Aula10_03/src/main/java/Piloto.java
@@ -0,0 +1,17 @@
+public class Piloto {
+
+
+    String nome;
+    boolean vilao;
+
+    void soltaSuperPoder(){
+        System.out.println("Piloto " + nome + "soltando super poder")
+
+    }
+    
+
+
+
+
+    
+}
