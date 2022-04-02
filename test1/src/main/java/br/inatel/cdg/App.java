068670d802c65eec39a388b3d487eec07e9ad7e4
VictorLaragnoit <victor.laragnoit@gmail.com>
2022-04-01 22:58:19 -0300

    teste1

---
+++ test1/src/main/java/br/inatel/cdg/App.java
@@ -0,0 +1,10 @@
+package br.inatel.cdg;
+import br.inatel.cdg.testes.teste1;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
