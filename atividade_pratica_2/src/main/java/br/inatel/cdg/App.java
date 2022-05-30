f8488ed456461472bdc6200ac074f66550cce170
VictorLaragnoit <victor.laragnoit@gmail.com>
2022-05-30 18:47:59 -0300

    main changes

--- atividade_pratica_2/src/main/java/br/inatel/cdg/App.java
+++ atividade_pratica_2/src/main/java/br/inatel/cdg/App.java
@@ -1,10 +1,27 @@
 package br.inatel.cdg;
 
+import br.inatel.cdg.gerencia.GerenciaFETIN;
+import br.inatel.cdg.projetos.Projeto;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Projeto projeto1 = new Projeto("Projeto 1", 76.00);
+        Projeto projeto2 = new Projeto("Projeto 2", 30.00);
+        Projeto projeto3 = new Projeto("Projeto 3", 88.50);
+        Projeto projeto4 = new Projeto("Projeto 4", 99.00);
+        GerenciaFETIN gerenciador = new GerenciaFETIN();
+
+        projeto3 = null;
+
+        gerenciador.addProjeto(projeto1);
+        gerenciador.addProjeto(projeto2);
+        gerenciador.addProjeto(projeto3);
+        gerenciador.addProjeto(projeto4);
+
+        gerenciador.listaProjetos();
+        System.out.println("Fim da execução");
     }
 }
